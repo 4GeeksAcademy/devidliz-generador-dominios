e3cff337640b65d8fde5d5e7e570769350871143
David <lizarte41@gmail.com>
2025-01-17 16:52:28 +0000

    Todo listo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const pronombres = ["el", "la", "los", "las"];
+const adjetivos = ["grande", "pequeño", "rapido", "lento"];
+const sustantivos = ["perro", "gato", "casa", "coche"];
+const extensiones = [".com", ".es", ".org"];
+
+function generarDominios() {
+  const resultadosDiv = document.getElementById("resultados");
+  resultadosDiv.innerHTML = "";
+
+  for (let i = 0; i < pronombres.length; i++) {
+    for (let j = 0; j < adjetivos.length; j++) {
+      for (let k = 0; k < sustantivos.length; k++) {
+        for (let l = 0; l < extensiones.length; l++) {
+          const dominio =
+            pronombres[i] + adjetivos[j] + sustantivos[k] + extensiones[l];
+          const listaul = document.createElement("ul");
+          listaul.textContent = dominio;
+          resultadosDiv.appendChild(listaul);
+        }
+      }
+    }
+  }
+}
+document
+  .getElementById("generarBtn")
+  .addEventListener("click", generarDominios);
